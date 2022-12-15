8b5ee797ece00380365a2c4ad15ae96168bd7d99
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-12-15 06:03:47 +0000

    Completed Lab05

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager()
+login.login_view = "login"
+login.init_app(app)
 
 # You must keep the routes at the end.
 from app import routes
