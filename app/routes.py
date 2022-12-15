2435fada11ebb8adbf8d8cd230fb79dbbcaf59d5
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-12-15 01:47:32 +0000

    Refactor into app package.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, redirect, flash
-from config import Config
+from flask import render_template, redirect, flash
 
-from forms import LoginForm
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
+from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
