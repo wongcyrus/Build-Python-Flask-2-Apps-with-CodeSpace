7a3d5bf643bf18d7984f6e62ed8864635d3b7603
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-12-15 07:19:54 +0000

    Complete Lab06

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from hashlib import md5
 from app import db, login
 
 from flask_login import UserMixin
@@ -13,6 +14,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow) 
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
@@ -22,6 +25,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 @login.user_loader
