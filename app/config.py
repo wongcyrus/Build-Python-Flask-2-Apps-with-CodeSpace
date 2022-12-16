fedb7637a65f52b53724d4cbbecd33691be3cdfc
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-12-16 07:26:34 +0000

    Migrate to postgres db

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'postgresql://postgres:postgres@postgresdb:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
