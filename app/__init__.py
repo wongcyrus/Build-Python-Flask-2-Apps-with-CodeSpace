e30812ded2d1701af900ae949f73c221cba17b83
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2022-12-19 07:13:14 +0000

    Completed Lab11

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login = LoginManager()
 login.login_view = "login"
 login.init_app(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     root = logging.getLogger()
