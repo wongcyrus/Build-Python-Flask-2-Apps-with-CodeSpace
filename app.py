7d48e78856f831b23aaf949549fff104364da229
Tom1114 <144003416+Tom1114@users.noreply.github.com>
2024-02-05 03:12:14 +0000

    Complete Lab03

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
+from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 @app.route("/")
 @app.route("/index")
@@ -19,3 +21,11 @@ def index():
     ]
 
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():   
+        flash('Login requested for user {}, remember_me {}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form = form)
